c686b2ec05fa11d295810e133ed9e80d05d4fc0b
Mukedlii <107921705+Mukedlii@users.noreply.github.com>
2025-11-20 23:33:41 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,23 +4,22 @@ module.exports = {
   // Telegram
   botToken: process.env.TELEGRAM_BOT_TOKEN,
   
-  // Hálózat beállítások (BASE MAINNET)
+  // Hálózat beállítások (BASE)
   network: {
     name: 'Base',
     rpcUrl: `https://base-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
     chainId: 8453,
-    symbol: 'ETH', // Base-en is ETH a fizetőeszköz, de Base ETH!
+    symbol: 'ETH',
     explorer: 'https://basescan.org'
   },
 
-  // Uniswap V2 Router (Base-en a Uniswap V2 vagy SushiSwap címe kell)
-  // Base Swap Router vagy Uniswap Universal Router
-  routerAddress: '0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24', // Uniswap Universal Router Base-en
+  // Router cím (Base Uniswap/Swap)
+  routerAddress: '0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24',
 
   // Díjak
   feeWallet: process.env.FEE_WALLET,
-  feePercent: 0.01, // 1% jutalék
-  minDeposit: 0.001, // Kisebb minimum befizetés (kb. 2-3 dollár)
+  feePercent: 0.01,
+  minDeposit: 0.001,
 
   // Biztonság
   encryptionKey: process.env.ENCRYPTION_KEY
